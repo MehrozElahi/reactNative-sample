1fbfe11cbc9d557a9d89b7f2d43472b60f5214e9
Mehroz Elahi <reach_melahi@dha.gov.ae>
2021-10-03 21:59:34 +0400

    first commit

---
+++ app/components/index.js
@@ -0,0 +1,34 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
+
+import LoadingScreen from './LoadingScreen'
+import HomeScreen from './Home'
+import NewQuoteScreen from './NewQuote'
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator({
+    Home:{
+        screen: HomeScreen,
+        navigationOptions: ({ navigation }) => ({
+            title: `Home`,
+        }),
+    },
+    NewQuote:{
+        screen: NewQuoteScreen,
+        navigationOptions: ({ navigation }) => ({
+            title: `New Quote`,
+        }),
+    }
+});
+
+const RoutesStack = createSwitchNavigator(
+    {
+        Loading: LoadingScreen,
+        App: AppStack
+    },
+    {initialRouteName: 'Loading'}
+);
+
+const Router = createAppContainer(RoutesStack);
+
+export default Router;
